c5ab8c20a840b2b6c7043dd005cd3b9f7a486db3
Andrew Babich <dearsa2015@mail.ru>
2017-07-22 15:36:23 +0300

    now symmetry check works in any string

--- Доп. задачи/7.php
+++ Доп. задачи/7.php
@@ -10,46 +10,61 @@
 <br><br>
 
      РЕШЕНИЕ:<br><br>
-		 $text = "1891001981";<br>
- 		$letters = preg_split('//', $text, -1, PREG_SPLIT_NO_EMPTY);<br>
- 		$count = 0;<br>
+		 $text = "129721";<br>
+		 $letters = preg_split('//', $text, -1, PREG_SPLIT_NO_EMPTY);<br>
+		 $count = 0;<br>
+		 $flag = false;<br>
 
- 		foreach ($letters as $key => $value) {<br>
- 		$count++;}<br>
+		 foreach ($letters as $key => $value) {<br>
+		 $count ++;}<br>
 
- 		if(is_int($count/2) == true){<br>
- 		for($i = 0; $i < $count; $i++) {<br>
- 		if($letters[$i] != $letters[$count - (1 + $i)]){<br>
- 		 echo "Строка не симметрична!";<br>
- 			break;<br>
- 		    }<br>
- 		if($i == $count/2){echo "Строка симметрична!";}<br>
- 		  }<br>
- 		}else{echo "Строка не симметрична!"; }<br>
+		 if(is_int($count/2) == true){<br>
+		 for($i = 0; $i < $count; $i++) {<br>
+		 if($letters[$i] != $letters[$count - (1 + $i)]){<br>
+		  $flag = true; echo "Строка не симметрична!";<br>
+		 	break;<br>
+		     }<br>
+		 }<br>
+		 }else{<br>
+		 	$fullCount = (($count/2) + 0.5) - 1; //индекс нечётного числа по середине.<br>
+		  for($i2 = 0; $i2 < $count; $i2++) {<br>
+		  if($letters[$i2] != $letters[($count -1) - $i2] && $i2 != ($fullCount)) {<br>
+		 	 $flag = true;  echo "Строка не симметрична!";<br>
+		 	 break;<br>
+		 		 }<br>
+		 	}<br>
+		 }<br>
+		  if($flag == false){echo "все хорошо, строка симметрична:)"; }<br>
 
 
 		ОТВЕТ: <br>
 <br>
 <?php
-
-$text = "1891001981";
+$text = "129721";
 $letters = preg_split('//', $text, -1, PREG_SPLIT_NO_EMPTY);
 $count = 0;
+$flag = false;
 
 foreach ($letters as $key => $value) {
-$count++;}
+$count ++;}
 
 if(is_int($count/2) == true){
 for($i = 0; $i < $count; $i++) {
 if($letters[$i] != $letters[$count - (1 + $i)]){
- echo "Строка не симметрична!";
+ $flag = true; echo "Строка не симметрична!";
 	break;
     }
-if($i == $count/2){echo "Строка симметрична!";}
-  }
-}else{echo "Строка не симметрична!"; }
-
-
+}
+}else{
+	$fullCount = (($count/2) + 0.5) - 1; //индекс нечётного числа по середине.
+ for($i2 = 0; $i2 < $count; $i2++) {
+ if($letters[$i2] != $letters[($count -1) - $i2] && $i2 != ($fullCount)) {
+	 $flag = true;  echo "Строка не симметрична!";
+	 break;
+		 }
+	}
+}
+ if($flag == false){echo "все хорошо, строка симметрична:)"; }
 ?>
    </body>
 <html>
