dd842c9b5f18b4407ab6af7c2bd4d55a6d40a66c
Andrew Babich <dearsa2015@mail.ru>
2017-07-22 15:14:59 +0300

    now task solved right way: gives 3 rows 3 colms

--- 16.php
+++ 16.php
@@ -5,26 +5,32 @@
 		<title>Задание 16</title>
   <head>
    <body>
-  16. Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9.
-	С помощью цикла foreach и оператора if выведите на экран
-	столбец элементов массива, как показано на картинке.(какой картинке?)
-	 1, 2, 3 4, 5, 6 7, 8, 9<br><br>
+		 16. Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9.
+	 	С помощью цикла foreach и оператора if выведите на экран
+	 	столбец элементов массива, как показано на картинке.<br><br>
 
-    РЕШЕНИЕ:<br>
-		$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];<br>
-		foreach ($arr as $value) {<br>
-		if($value == 3){echo $value;}else{echo "$value br>";}<br>
-		}<br>
-		 <br><br>
+     РЕШЕНИЕ:<br><br>
+
+
+ 		$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];<br>
+ 		foreach ($arr as $value) {<br>
+ 		echo $value, " ";<br>
+ 		if(is_int($value/3)){ echo "/br>";}<br>
+ 		}<br>
 
 
 		ОТВЕТ: <br>
-<?php
-$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-foreach ($arr as $value) {
-if($value == 3){echo $value;}else{echo "$value <br>";}
-}
-?>
+		<?php
+		$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		foreach ($arr as $value) {
+		echo $value, " ";
+		if(is_int($value/3)){ echo "</br>";}
+		}
+		?>
+		   </body>
+		<html>
+
+
    </body>
 <html>
