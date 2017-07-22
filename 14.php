c8811736aa710f8fcfcd558b474cf636b3b2211a
Andrew Babich <dearsa2015@mail.ru>
2017-07-22 15:13:02 +0300

    fixed: now echo gives only one answer

--- 14.php
+++ 14.php
@@ -5,42 +5,40 @@
 		<title>Задание 14</title>
   <head>
    <body>
-    14. Дан массив с элементами 4, 2, 5, 19, 13, 0, 10.
+		 14. Дан массив с элементами 4, 2, 5, 19, 13, 0, 10.
 		С помощью цикла foreach и оператора if проверьте есть
 		ли в массиве элемент со значением $e, равном 2, 3 или 4.
 		Если есть — выведите на экран 'Есть!', иначе выведите 'Нет!'.<br><br>
-    РЕШЕНИЕ:<br>
-		$arr = [4, 2, 5, 19, 13, 0, 10];<br>
-		$e = [2, 3, 4];<br>
-		$check = false;<br>
-		foreach ($arr as $value) {<br>
-		  $check = false;<br>
-			foreach ($e as $value2){<br>
-				if($value == $value2){ $check = true;} //если массив $е содержит хотяб один элмент массива $arr<br>
-			}<br>
-			if($check == true){echo "Есть!", "  ";}else{echo "Нет!", "  ";} <br>
-		}<br>
 
-		 <br><br>
+     РЕШЕНИЕ:<br><br>
+		 $arr = [4, 2, 5, 19, 13, 0, 10];<br>
+		 $e = [2, 3, 4];<br>
+		 $check = false;<br>
+		 foreach ($arr as $value) {<br>
+		 	foreach ($e as $value2){<br>
+		 		if($value == $value2){ $check = true;}<br>
+		 	}<br>
 
+		 }<br>
+		 if($check == true){echo "Есть!";}else{echo "Нет!";}<br>
 
 		ОТВЕТ: <br>
-<?php
-$arr = [4, 2, 5, 19, 13, 0, 10];
-$e = [2, 3, 4];
-$check = false;
-foreach ($arr as $value) {
-  $check = false;
-	foreach ($e as $value2){
-		if($value == $value2){ $check = true;} //если массив $е содержит элмент массива $arr
-	}
-	if($check == true){echo "Есть!", "  ";}else{echo "Нет!", "  ";}
-}
 
-?>
+		<?php
+		$arr = [4, 2, 5, 19, 13, 0, 10];
+		$e = [2, 3, 4];
+		$check = false;
+		foreach ($arr as $value) {
+			foreach ($e as $value2){
+				if($value == $value2){ $check = true;} //если массив $е содержит элмент массива $arr
+			}
 
+		}
+		if($check == true){echo "Есть!";}else{echo "Нет!";}
 
-
+		?>
+		   </body>
+		<html>
 
 
    </body>
