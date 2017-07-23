83b3e24256b1da05c3aaa0ecb50fff72c61f7e2c
Andrew Babich <dearsa2015@mail.ru>
2017-07-23 13:14:10 +0300

    string has been created

--- 8.php
+++ 8.php
@@ -19,9 +19,11 @@
 
 		<?php
 			$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+			$str = "";
 			foreach ($arr as $key => $value) {
-					echo $value;
+					$str = $str. $value;
 			}
+			echo $str;
 		?>
 
    </body>
