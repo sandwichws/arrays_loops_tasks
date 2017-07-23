005a22d80b2fc40822b6878d0282000e4c8272eb
Andrew Babich <dearsa2015@mail.ru>
2017-07-23 13:29:49 +0300

    useless if block has been removed

--- Доп. задачи/7.php
+++ Доп. задачи/7.php
@@ -18,14 +18,14 @@
 		 foreach ($letters as $key => $value) {<br>
 		 $count ++;}<br>
 
-		 if(is_int($count/2) == true){<br>
+
 		 for($i = 0; $i < $count; $i++) {<br>
 		 if($letters[$i] != $letters[$count - (1 + $i)]){<br>
 		  $flag = true; echo "Строка не симметрична!";<br>
 		 	break;<br>
 		     }<br>
 		 }<br>
-		 }else{<br>
+
 		 	$fullCount = (($count/2) + 0.5) - 1; //индекс нечётного числа по середине.<br>
 		  for($i2 = 0; $i2 < $count; $i2++) {<br>
 		  if($letters[$i2] != $letters[($count -1) - $i2] && $i2 != ($fullCount)) {<br>
@@ -33,14 +33,14 @@
 		 	 break;<br>
 		 		 }<br>
 		 	}<br>
-		 }<br>
+
 		  if($flag == false){echo "все хорошо, строка симметрична:)"; }<br>
 
 
 		ОТВЕТ: <br>
 <br>
 <?php
-$text = "129721";
+$text = "1221";
 $letters = preg_split('//', $text, -1, PREG_SPLIT_NO_EMPTY);
 $count = 0;
 $flag = false;
@@ -48,14 +48,14 @@ $flag = false;
 foreach ($letters as $key => $value) {
 $count ++;}
 
-if(is_int($count/2) == true){
+
 for($i = 0; $i < $count; $i++) {
 if($letters[$i] != $letters[$count - (1 + $i)]){
  $flag = true; echo "Строка не симметрична!";
 	break;
-    }
+		}
 }
-}else{
+
 	$fullCount = (($count/2) + 0.5) - 1; //индекс нечётного числа по середине.
  for($i2 = 0; $i2 < $count; $i2++) {
  if($letters[$i2] != $letters[($count -1) - $i2] && $i2 != ($fullCount)) {
@@ -63,7 +63,7 @@ if($letters[$i] != $letters[$count - (1 + $i)]){
 	 break;
 		 }
 	}
-}
+
  if($flag == false){echo "все хорошо, строка симметрична:)"; }
 ?>
    </body>
